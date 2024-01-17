c94f142b415369406bf11963a0847755b8e65a05
Inu-kamii <SashaKasperchuk2@gmail.com>
2024-01-17 15:47:15 +0000

    Fixed main flask server

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-import os
-
-from dotenv import load_dotenv
-from flask import Flask
-from flask_restful import Api
 import pip
 
-pip.main(['install', 'flask'])
-pip.main(['install', 'Flask-RESTful'])
 pip.main(['install', 'psycopg2-binary'])
-pip.main(['install', 'python-dotenv'])
 
+from flask import Flask
+from flask_restful import Api
+from threading import Thread
 
 from controllers.messages_controller import MessagesController
 
 app = Flask('')
 api = Api()
 
-load_dotenv()
-
 api.add_resource(MessagesController, '/api/notes')
 api.init_app(app)
+
 
 @app.route('/')
 def home():
   return "I'm alive"
 
-if __name__ == '__main__':
-    app.run(debug=True, port=3000, host='localhost')
+
+def run():
+  app.run(host='0.0.0.0', port=80)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+keep_alive()
